f02c09f5eaba5f76f6f61c7a33e76e0bb4e20e08
pedromarquetti <pedro.marquettti@gmail.com>
2025-01-06 15:53:11 -0300

    update main script - added basic implementation of graph plot

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
 import json
+import math
 import os
 import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 from pandas.core.api import DataFrame
 
-DEFAULT_DIR = './data/'
-WRITE_FILE = './appended_data.json'
+dir = './data/'
+write_file = './appended_data.json'
 # number of rows to fetch
-NUMBER_OF_ROWS = 100
-
-
-# change max rows before truncating
-pd.set_option('display.max_rows', NUMBER_OF_ROWS)
-pd.set_option('display.max_colwidth', None)
-pd.set_option('display.max_seq_items', None)
-
+number_of_rows = 10
 data = []
 
 
-def read_files(path:str):
+def read_files():
     """Helper function that reads folder for JSON files containing Spotify data and saves it to a global data array"""
-    directory = os.fsencode(path)
+    directory = os.fsencode(dir)
 
     for file in os.listdir(directory):
         filename = os.fsdecode(file)
 
         if filename.endswith('.json'):
 
-            with open(f'{path}/{filename}') as fread:
+            with open(f'{dir}/{filename}') as fread:
                 json_data = json.load(fread)
                 data.extend(json_data)
 
 def write_to_file():
-    with open(WRITE_FILE, 'w') as fwrite:
+    with open(write_file, 'w') as fwrite:
         fwrite.write(f'{data}')
 
 def print_stats(df:DataFrame):
@@ -42,29 +36,41 @@ def print_stats(df:DataFrame):
     print(f'Total listen time:')
     print(listen_time["mins_played"].sum())
 
-    print(f'\nTop {NUMBER_OF_ROWS} artists: ')
-    print(listen_time['master_metadata_album_artist_name'].value_counts().head(NUMBER_OF_ROWS).to_string(header=False,index=True))
+    print(f'\nTop {number_of_rows} artists (count of songs played by artist): ')
+    print(listen_time['artist'].value_counts().head(number_of_rows).to_string(header=False,index=True))
 
-    min_by_artist = df.groupby('master_metadata_album_artist_name')[
+    min_by_artist = df.groupby('artist')[
         'mins_played'].sum()
     print('\n Minutes played per artist')
-    print(min_by_artist.sort_values(ascending=False).head(NUMBER_OF_ROWS).to_string(header=False,index=True))
+    print(min_by_artist.sort_values(ascending=False).head(number_of_rows).to_string(header=False,index=True))
     
-    min_by_song = df.groupby('master_metadata_track_name').aggregate(
-        {'master_metadata_album_artist_name':'first',
+    min_by_song = df.groupby('track').aggregate(
+        {'artist':'first',
             'mins_played':'sum'}
     )
-    print('\n Minutes played per song')
-    print(min_by_song.sort_values(by='mins_played',ascending=False).head(NUMBER_OF_ROWS).to_string(header=False))
+    print('\nMinutes played per song')
+    print(min_by_song.sort_values(by='mins_played',ascending=False).head(number_of_rows).to_string(header=False,index=True))
 
+#TODO: implement graph plotting func
 def plot_graphs(df:DataFrame):
 
     # listen_time['master_metadata_album_artist_name'].value_counts().head(n).plot(kind='bar',title=f'top {n} artists of all time')
     # plt.show()
     pass
 
-def main(mode:str,filepath=DEFAULT_DIR):
-    read_files(filepath)
+def artist_by_year_graph(df:DataFrame):
+    yearly =df.groupby(df['year'])['mins_played'].sum()
+    _,ax = plt.subplots()
+    yearly.plot(kind='bar',title='Minutes played by year',ylabel='Minutes played',xlabel='year')
+    for i,v in enumerate(yearly):
+        ax.text(i,v,str(v),ha='center',va='bottom')
+    plt.show()
+
+def main(mode:str):
+    if len(data) == 0:
+        # if user specifies multiple flags, dont keep appending to data if it already contains values
+        read_files()
+
     # global DataFrame that will be used by all functions
     df = pd.DataFrame(data)
 
@@ -76,26 +82,45 @@ def main(mode:str,filepath=DEFAULT_DIR):
     df['mins_played'] = df['ms_played'] / 60000
     df['hours_played'] = df['mins_played'] / 60
 
+    # renaming columns
+    df.rename({
+        'master_metadata_track_name':'track',
+        'master_metadata_album_album_name':'album',
+        'master_metadata_album_artist_name':'artist'
+    },errors='raise',axis='columns',inplace=True)
+
     # TODO: add more modes 
     match mode:
-        # Stats mode
-        case 's':
+        case 'stats':
+            # INFO: Stats mode
             print_stats(df)
-
+        case 'top-plot':
+            artist_by_year_graph(df)
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Spotify JSON data analyser script')
     parser.add_argument('-s',action='store_true',help='Runs script in Stats mode (this mode prints top songs/artists)')
-    parser.add_argument('-d','--dir', action='store_true',help='Specifies a folder containing unziped Spotify data (defaults to ./data/)')
+    parser.add_argument('-t','--top-plot',action='store_true',help='plot top artists graph')
 
-    d = DEFAULT_DIR
+    parser.add_argument('-d', type=str,help='Specifies a folder containing unziped Spotify data (defaults to ./data/)')
+    parser.add_argument('-n',type=int,help='Specified number of rows to show')
 
     args = parser.parse_args()
 
     if args.d:
-        d = args.d
+        # change default dir
+        dir = args.d
+
+    if args.n:
+        number_of_rows = args.n
 
     if args.s:
-        main('s',d)
+        main('stats')
+        # using pass to accept multiple flags
+        pass
+    if args.top_plot :
+        main('top-plot')
+        pass
+
 
